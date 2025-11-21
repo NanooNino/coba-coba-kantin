107357c3373958e34ca58614697b7920edd95d9f
NanooNino <162066987+NanooNino@users.noreply.github.com>
2025-11-21 22:08:44 +0800

    Delete app directory

--- app/themes/admin/partials/loading.php
+++
@@ -1,8 +0,0 @@
-<div id="loadingModal" class="modal-loading">
-  <div class="modal-loading-content">
-      <div class="base-spinner">
-        <div class="spinner-loader"></div>
-      </div>
-      <p id="text_loading">Tunggu sebentar...</p>
-  </div>
-</div>
